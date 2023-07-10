4330081d36f3c9c335e52396e7407177efbecbdb
Joshua Crews <86257459+joshua-crews@users.noreply.github.com>
2023-07-10 15:57:35 +0100

    Added swagger docs for backend api endpoints

--- src/ECFSystem/ECFSystem/urls.py
+++ src/ECFSystem/ECFSystem/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', get_schema_view(title='API Schema', description='REST API endpoints'), name='api'),
+    path('docs/', TemplateView.as_view(template_name='docs.html', extra_context={'schema_url':'api'}), name='swagger-ui'),
 ]
